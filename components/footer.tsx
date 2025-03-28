ba402ca85e52e31f384ea0293f72cc431f1f11b7
Henrik Nilsen-Lomo <henriklomo@gmail.com>
2025-03-28 22:41:50 +0100

    add footer

---
+++ components/footer.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export function Footer() {
+    return (
+        <footer className="fixed bottom-0 w-full bg-foreground text-background p-6 border-t flex">
+            <p className="uppercase text-lg font-bold">
+                ...KOMPIS
+            </p>
+            <p className="ml-auto flex justify-center">
+                Laget av Ninja Turles 🐢 Mille, Karolina, Johanne og Henrik
+            </p>
+        </footer>
+    );
+}

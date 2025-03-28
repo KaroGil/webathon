cc2fbe9f07d6bc941b2bebd2ce99e0ccd3597c78
Johanne Blikberg Herheim <joher2128@uib.no>
2025-03-28 18:42:49 +0100

    Add header

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
+import { Header } from "@/components/header";
+
 export default function Home() {
-  return (
-    <div className="w-full h-12 flex items-center bg-blue-200 border-b">
-      <p className="mx-6">Badekompis</p>
-    </div>
-  );
+  return <Header />;
 }
---
+++ components/header.tsx
@@ -0,0 +1,7 @@
+export function Header() {
+  return (
+    <div className="w-full h-12 flex items-center bg-blue-200 border-b">
+      <p className="mx-6">Badekompis</p>
+    </div>
+  );
+}

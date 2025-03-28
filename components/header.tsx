f9bebc6becb29a6cb82bd85cc8996088879145e2
Johanne Blikberg Herheim <145448180+johanneherheim@users.noreply.github.com>
2025-03-28 19:23:19 +0100

    Add link to home
    Co-authored-by: Karolina Gil <60822728+KaroGil@users.noreply.github.com>

--- components/header.tsx
+++ components/header.tsx
@@ -4,7 +4,7 @@ import { hobbies } from "./hobbies";
 export function Header() {
   return (
     <div className="w-full h-12 flex px-5 items-center justify-between bg-blue-200 border-b">
-      <p className="">BADEKOMPIS</p>
+       <Link href="/">BADEKOMPIS</Link>
       <div className="space-x-4">
         {hobbies.map((h: string) => (
           <Link key={h} href={`/${h}`}>

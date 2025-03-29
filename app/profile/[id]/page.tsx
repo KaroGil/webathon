4685230ef9823eace822b79837b9a75d1a71618f
Johanne Blikberg Herheim <joher2128@uib.no>
2025-03-29 21:44:24 +0100

    Fix grid

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -21,7 +21,7 @@ export default function ProfilePage() {
       <div>
         <h2 className="text-2xl font-semibold">Mine arrangementer:</h2>
         {myEvents.length > 0 ? (
-          <ul className="space-y-2 mt-2 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+          <ul className="space-y-2 mt-2 grid grid-cols-1 md:grid-cols-2  gap-4">
             {myEvents.map((event) => (
               <Post
                 key={event.date.toISOString()}

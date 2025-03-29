450b6efb7a46571e2633b4aa1db26f9221d71fbc
KaroGil <karolinkagil@gmail.com>
2025-03-29 15:09:01 +0100

    add add interest page

---
+++ app/hobbies/add/page.tsx
@@ -0,0 +1,8 @@
+export default function AddInterest() {
+  return (
+    <div className="flex flex-col gap-8 items-center justify-center m-6 font-bold text-xl">
+      <h1>Legg til en interesse</h1>
+      <p className="text-center font-lg">Kommer snart!</p>
+    </div>
+  );
+}

8221beb9117c89f411df8e336c04dcf52d473ddb
KaroGil <karolinkagil@gmail.com>
2025-03-29 13:02:34 +0100

    add footer link and aboutus page

---
+++ app/aboutUs/page.tsx
@@ -0,0 +1,14 @@
+export default function AboutUs() {
+  return (
+    <div className="flex flex-col items-center justify-center h-full">
+      <h1 className="text-4xl font-bold mb-4">Om oss</h1>
+      <p className="text-lg mb-2">Vi er KOMPIS, et prosjekt laget av:</p>
+      <ul className="list-disc list-inside">
+        <li>Mille</li>
+        <li>Karolina</li>
+        <li>Johanne</li>
+        <li>Henrik</li>
+      </ul>
+    </div>
+  );
+}
--- components/footer.tsx
+++ components/footer.tsx
@@ -1,8 +1,15 @@
+import Link from "next/link";
+
 export function Footer() {
   return (
-    <footer className="fixed bottom-0 w-full bg-foreground text-background p-6 border-t flex">
-      <p className="uppercase text-lg font-bold">...KOMPIS</p>
-      <p className="ml-auto flex justify-center">
+    <footer className="flex flex-col bottom-0 w-full bg-foreground text-background p-12 border-t">
+      <div className="flex flex-row items-center justify-center w-full">
+        <p className="uppercase text-lg font-bold">...KOMPIS</p>
+        <Link href="/aboutUs" className="ml-auto flex justify-center">
+          Om oss
+        </Link>
+      </div>
+      <p className="mt-10 flex justify-center">
         Laget av Ninja Turtles 🐢 Mille, Karolina, Johanne og Henrik
       </p>
     </footer>

f57af9c024b762dd892a42ef27838889fcae0474
Karolina Gil <60822728+KaroGil@users.noreply.github.com>
2025-03-29 15:41:43 +0100

    Dynamic header (#14)
    * add a page where you can see all interests
    
    * add add interest page
    
    * add dynamci header to switch based on insterest
    
    * remove old headeing from hobby pages
    
    * change get function

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,20 @@
+"use client";
+
 import Link from "next/link";
 import AuthButtons from "@/components/authButtons";
+import { usePathname } from "next/navigation";
+import { getInterestBySlug } from "./interests";
 
 export function Header() {
+  const pathname = usePathname();
+
+  let title = "...KOMPIS";
+
+  if (pathname.startsWith("/hobbies/")) {
+    const hobby = pathname.split("/")[2];
+    const name = getInterestBySlug(hobby);
+    title = `${name?.infinitiv?.toUpperCase()}kompis`;
+  }
   return (
     <header className="w-full bg-black text-background px-4 py-12">
       <div className="max-w-7xl mx-auto flex flex-col md:flex-row items-center md:items-center justify-between relative">
@@ -9,7 +22,7 @@ export function Header() {
           href="/"
           className="uppercase font-bold text-5xl text-center md:absolute md:left-1/2 md:transform md:-translate-x-1/2"
         >
-          ...KOMPIS
+          {title}
         </Link>
 
         <div className="mt-4 md:mt-0 md:ml-auto">

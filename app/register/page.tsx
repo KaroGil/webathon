279a2cd6eb3ef6e367aca216c224cb9c13dcc364
KaroGil <karolinkagil@gmail.com>
2025-03-29 14:35:58 +0100

    add register form

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -5,7 +5,9 @@ import { useState } from "react";
 
 export default function RegisterPage() {
   const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
   const [password, setPassword] = useState("");
+  const [repeatPassword, setRepeatPassword] = useState("");
   const [error, setError] = useState("");
 
   async function handleSubmit(e: React.FormEvent) {
@@ -27,12 +29,19 @@ export default function RegisterPage() {
   }
 
   return (
-    <div className="flex items-center justify-center mt-40">
+    <div className="flex items-center justify-center mt-16">
       <form
         onSubmit={handleSubmit}
         className="flex flex-col gap-4 items-center w-72"
       >
         <h1 className="text-2xl font-bold mb-8">Registrer deg</h1>
+        <input
+          className="border p-2 rounded w-full"
+          type="Navn"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Navn"
+        />
         <input
           className="border p-2 rounded w-full"
           type="Email"
@@ -47,12 +56,19 @@ export default function RegisterPage() {
           onChange={(e) => setPassword(e.target.value)}
           placeholder="Passord"
         />
+        <input
+          className="border p-2 rounded w-full"
+          type="Password"
+          value={repeatPassword}
+          onChange={(e) => setRepeatPassword(e.target.value)}
+          placeholder="Gjenta passord"
+        />
         {error && <p className="text-red-500">{error}</p>}
         <button
           className="bg-black text-white px-4 py-2 rounded w-full"
           type="submit"
         >
-           Register deg
+          Register deg
         </button>
       </form>
     </div>

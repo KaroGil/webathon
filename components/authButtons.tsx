a799d63bf5c912e44d4d1324ab72bf99df55b64b
Karolina Gil <60822728+KaroGil@users.noreply.github.com>
2025-03-29 14:24:38 +0100

    restyle header (#10)

--- components/authButtons.tsx
+++ components/authButtons.tsx
@@ -9,7 +9,7 @@ export default function AuthButtons() {
     return (
       <button
         onClick={() => signOut({ callbackUrl: "/" })}
-        className="bg-background text-foreground px-4 py-2 rounded mx-4 w-30 font-bold"
+        className={`bg-background text-foreground px-4 py-2 rounded mx-4 w-30 font-bold hover:bg-foreground hover:text-background transition-all duration-300 `}
       >
         Logg ut
       </button>
@@ -19,7 +19,7 @@ export default function AuthButtons() {
   return (
     <button
       onClick={() => signIn()}
-      className="bg-background text-foreground px-4 py-2 rounded mx-4 w-30 font-bold"
+      className={`bg-background text-foreground px-4 py-2 rounded mx-4 w-30 font-bold hover:bg-foreground hover:text-background round transition-all duration-300 `}
     >
       Logg inn
     </button>

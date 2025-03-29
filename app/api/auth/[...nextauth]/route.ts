225258f4042b1f49dedd5d3e2abd81de3d7e21fa
Karolina Gil <60822728+KaroGil@users.noreply.github.com>
2025-03-29 12:38:33 +0100

    Update route.ts (#8)
    * Update route.ts
    
    * Create authOptions
    
    * Update route.ts
    
    * Update route.ts
    
    * Update route.ts
    
    * Update tsconfig.json
    
    * Update route.ts
    
    * Create authOptions.ts
    
    * Update authOptions
    
    * Update authOptions.ts
    
    * Update page.tsx
    
    * delete authMobile textfile
    
    * add .env.example file

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,40 +1,5 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-
-export const authOptions: NextAuthOptions = {
-  providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: { label: "Email", type: "email" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials) {
-        const user = {
-          id: "1",
-          name: "Ole Brumm",
-          email: "OleBrumm@example.com",
-        };
-
-        if (
-          credentials?.email === "OleBrumm@example.com" &&
-          credentials?.password === "pass123"
-        ) {
-          return user;
-        }
-
-        return null;
-      },
-    }),
-  ],
-  pages: {
-    signIn: "/login",
-  },
-  session: {
-    strategy: "jwt",
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-};
+import NextAuth from "next-auth";
+import { authOptions } from "@lib/authOptions";
 
 const handler = NextAuth(authOptions);
 

a24d2e78f6f94a007f7c2d37860887ccfbaf2f6b
Karolina Gil <60822728+KaroGil@users.noreply.github.com>
2025-03-29 21:21:30 +0100

    add ducks (#26)

--- components/header.tsx
+++ components/header.tsx
@@ -5,23 +5,37 @@ import AuthButtons from "@/components/authButtons";
 import { usePathname } from "next/navigation";
 import { getInterestBySlug } from "./types/interests";
 import { getEventById } from "./types/post";
+import { useEffect, useState } from "react";
+import { AnimatedIcons } from "@/components/animation/animatedIcons";
 
 export function Header() {
   const pathname = usePathname();
 
-  let title = "...KOMPIS";
+  const [bade, setBade] = useState(false);
+  const [title, setTitle] = useState("...KOMPIS");
 
-  if (pathname.startsWith("/hobbies/")) {
-    const hobby = pathname.split("/")[2];
-    const name = getInterestBySlug(hobby);
-    title = `${name?.infinitiv?.toUpperCase()}kompis`;
-  }
-  if (pathname.startsWith("/event/")) {
-    const id = pathname.split("/")[2];
-    const event = getEventById(parseInt(id));
-    title = `${event?.hobby.infinitiv}kompis`;
-  }
-  return (
+  useEffect(() => {
+    if (pathname.startsWith("/hobbies/")) {
+      const hobby = pathname.split("/")[2];
+      const name = getInterestBySlug(hobby);
+      console.log("Got hobby name:", name);
+
+      setTitle(`${name?.infinitiv?.toUpperCase()}kompis`);
+      if (name?.name == "Bading") {
+        setBade(true);
+      }
+    } else if (pathname.startsWith("/event/")) {
+      const id = pathname.split("/")[2];
+      const event = getEventById(parseInt(id));
+      setTitle(`${event?.hobby.infinitiv}kompis`);
+      setBade(false);
+    } else {
+      setTitle("...KOMPIS");
+      setBade(false);
+    }
+  }, [pathname]);
+
+  const headerContent = (
     <header className="w-full bg-black text-background px-4 py-12">
       <div className="max-w-7xl mx-auto flex flex-col md:flex-row items-center md:items-center justify-between relative">
         <Link
@@ -36,5 +50,13 @@ export function Header() {
         </div>
       </div>
     </header>
+  );
+
+  return bade ? (
+    <AnimatedIcons n={10} bade={bade}>
+      {headerContent}
+    </AnimatedIcons>
+  ) : (
+    headerContent
   );
 }

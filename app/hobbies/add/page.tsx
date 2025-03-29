7680e237d834dc18c4392254bd570f1a58ef0404
Johanne Blikberg Herheim <joher2128@uib.no>
2025-03-29 16:02:08 +0100

    Move button

--- app/hobbies/add/page.tsx
+++ app/hobbies/add/page.tsx
@@ -1,8 +1,17 @@
+import LinkButton from "@/components/linkButton";
+
 export default function AddInterest() {
   return (
-    <div className="flex flex-col gap-8 items-center justify-center m-6 font-bold text-xl">
-      <h1>Legg til en interesse</h1>
+    <div className="flex flex-col gap-10 items-center justify-center m-6 font-bold text-xl">
       <p className="text-center font-lg">Kommer snart!</p>
+      <p className="text-center font-lg flex flex-col gap-2">
+        Savner du noe?
+        <LinkButton
+          url="/hobbies/add"
+          text="Legg til en interesse"
+          className="text-sm"
+        />
+      </p>
     </div>
   );
 }

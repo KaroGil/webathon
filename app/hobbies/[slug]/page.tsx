6ea9cefa323a0fc8446ce449a942e6334f8e86e8
KaroGil <karolinkagil@gmail.com>
2025-03-30 11:08:00 +0200

    space it out morespace it out more

--- app/hobbies/[slug]/page.tsx
+++ app/hobbies/[slug]/page.tsx
@@ -13,7 +13,7 @@ export default async function HobbyPage({ params }: PageProps) {
   const filteredPosts = posts.filter((post) => post.hobby?.slug === slug);
 
   return (
-    <div className="relative p-4 w-full space-y-4">
+    <div className="relative px-4 pt-12 pb-40 w-full space-y-4">
       <NewPost />
       <div className="w-full grid grid-cols-1 gap-4 md:grid-cols-2 ">
         {filteredPosts.map((post) => (

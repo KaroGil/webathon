135b0bbd153651512ffeafa92bd236c5b8cc1e5c
Johanne Blikberg Herheim <joher2128@uib.no>
2025-04-28 11:13:37 +0200

    Fix form

--- app/actions/add-interest.ts
+++ app/actions/add-interest.ts
@@ -5,7 +5,7 @@ import { redirect } from "next/navigation";
 export async function addInterest(formData: FormData) {
   const supabase = await createClient();
 
-  const name = formData.get("name") as string;
+  const name = formData.get("interest") as string;
   const infinitiv = formData.get("infinitiv") as string;
   const slug = generateSlug(name);
 
